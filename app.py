4b73f8b94989299f62b87ce75fdede696592e237
Gary Haith <gary.haith@gmail.com>
2021-08-19 10:15:02 -0600

    Added annotations to cumulative plot

--- app.py
+++ app.py
@@ -398,7 +398,7 @@ with st.expander(f"Cumulative Costs"):
     cum_cost_fig.update_layout(legend=dict( orientation="v", yanchor="top", y=0.99, xanchor="left", x=0.01))
     cum_cost_fig.add_vline(x=59, line_width=2, line_dash="dash", line_color="black")
     cum_cost_fig.add_vline(x=23, line_width=2, line_dash="dash", line_color="blue")
-    cum_cost_fig.add_trace(go.Scatter(x=[24],y=[cost.cumComp.max() * .9],mode="text",text=["Fully Installed"],textposition="middle right",showlegend = False, textfont=dict(
+    cum_cost_fig.add_trace(go.Scatter(x=[24],y=[cost.cumComp.max() * .9],mode="text",text=[f"{num_facs_for_rollout} Installed"],textposition="middle right",showlegend = False, textfont=dict(
         family="sans serif",
         size=14,
         color="Blue"
